89c596fbc88f2b977c8eec037b3bd065db837255
jesusvaz <jesusvaz@gmail.com>
2026-04-13 11:33:48 -0600

    add and update content

--- src/app/data/treatments.ts
+++ src/app/data/treatments.ts
@@ -6,30 +6,30 @@ export const treatments = [
         description:
             "Tratamientos faciales diseñados para limpiar, hidratar y revitalizar la piel, mejorando su apariencia y salud."
     },
-    {
-        slug: "rejuvenecimiento",
-        title: "Rejuvenecimiento",
-        image: "rejuvenecimientom1.png",
-        description:
-            "Procedimientos enfocados en reducir signos de envejecimiento, estimulando la producción de colágeno y elastina."
-    },
+    // {
+    //     slug: "rejuvenecimiento",
+    //     title: "Rejuvenecimiento",
+    //     image: "rejuvenecimientom1.png",
+    //     description:
+    //         "Procedimientos enfocados en reducir signos de envejecimiento, estimulando la producción de colágeno y elastina."
+    // },
     {
         slug: "derma-pen",
-        title: "Derma Pen",
+        title: "Derma-Pen",
         image: "termapenm1.png",
         description:
             "Técnica de microneedling que mejora la textura de la piel, atenúa cicatrices y líneas de expresión."
     },
-    {
-        slug: "fibro-blast",
-        title: "Fibro Blast",
-        image: "fivbroblastm1.png",
-        description:
-            "Tratamiento no invasivo que ayuda a reafirmar la piel y mejorar su firmeza mediante estimulación celular."
-    },
+    // {
+    //     slug: "fibro blast",
+    //     title: "Fibro-Blast",
+    //     image: "fivbroblastm1.png",
+    //     description:
+    //         "Tratamiento no invasivo que ayuda a reafirmar la piel y mejorar su firmeza mediante estimulación celular."
+    // },
     {
         slug: "plasma-pen",
-        title: "Plasma Pen",
+        title: "Plasma-Pen",
         image: "plasmapenm1.png",
         description:
             "Procedimiento estético avanzado que ayuda a tensar la piel y mejorar arrugas sin cirugía."
@@ -37,23 +37,36 @@ export const treatments = [
     {
         slug: "depilacion-tri-laser",
         title: "Depilación",
-        image: "depilacionm1.png",
+        image: "depilacion.png",
         description:
             "Sistema de depilación láser que combina tres longitudes de onda para una eliminación eficaz del vello."
     },
     {
         slug: "eliminacion-verrugas",
         title: "Verrugas",
-        image: "verugam1.png",
+        image: "verrugas.png",
         description:
             "Tratamiento seguro y efectivo para remover verrugas y lesiones cutáneas de forma controlada."
     },
-   
-     {
+
+    {
         slug: "Ventosas",
         title: "Ventosas",
         image: "ventosasm1.png",
         description:
             "Procedimiento estético que da volumen, definición e hidratación a los labios de manera natural."
+    },
+    {
+        slug: "Barber",
+        title: "Barber",
+        image: "barber.png",
+        description:
+            "Corte de cabello, afeitado, arreglo de barba, perfilado, degradado (fade), diseño de cejas, tratamientos capilares, lavado de cabello y asesoría de estilo."
+    },
+    {
+        slug: "Manicure0-Pedicure",
+        title: "Manicure-Pedicure",
+        image: "uñas.png",
+        description:"Manicure, pedicure, esmaltado regular y gel, uñas acrílicas, diseño de uñas (nail art), retiro de esmalte, limpieza y cuidado de cutículas, exfoliación, hidratación y masajes de manos y pies."
     }
 ];
